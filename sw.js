0bbdd04a525b79c5171ed2babe878913b5b43476
Igor Estevam <igorateu888@gmail.com>
2026-07-19 12:54:34 -0300

    Bug scroll de dedo no celular

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 // todos os clientes abertos recarregam automaticamente.
 // =============================================
 
-const VERSION = 'v26';
+const VERSION = 'v27';
 const STATIC_CACHE = `lr-static-${VERSION}`;
 const RUNTIME_CACHE = `lr-runtime-${VERSION}`;
 
